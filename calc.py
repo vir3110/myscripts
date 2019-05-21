de247a5368028c4c96b0c382ade125bdc08edf1e
gitman <virendratm@gmail.com>
2019-05-21 08:45:17 +0530

    Final chages

--- calc.py
+++ calc.py
@@ -3,12 +3,9 @@ def add(x,y):
 def subtract(x,y):
     pass
 def multiply(x,y):
-    pass
+    return x*y
 def divide(x,y):
     pass
 def square(x,y):
     pass
-
-def multiply(x,y):
-    return x*y
 
